188e1c7e4d7d2993fed1d303181bd5ab528cb80a
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-03 12:52:37 +0100

    Agregar footer global de la aplicación

---
+++ src/app/shared/components/app-footer/app-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-app-footer',
+  imports: [],
+  templateUrl: './app-footer.component.html',
+})
+export class AppFooterComponent {}
--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AppFooterComponent } from '../../components/app-footer/app-footer.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
