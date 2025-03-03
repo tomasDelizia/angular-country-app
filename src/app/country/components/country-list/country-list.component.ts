c197fdba407f74ed16fa3b596d92d2c718ff6b2b
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-03 16:30:11 +0100

    Agregar DecimalPipe para mostrar la población

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'country-list',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './country-list.component.html',
 })
 export class CountryListComponent {
