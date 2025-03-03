709450b60d62a134a6f7ded9d2b6eb208703fb4e
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-03 13:54:00 +0100

    Aislar componentes CountrySearchInput y CountryList

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { CountrySearchInputComponent } from '../../components/top-menu/country-search-input/country-search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [CountrySearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent {}
+export class ByCapitalPageComponent {
+  onSearch(value: string) {
+    console.log(value);
+  }
+}
--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { CountrySearchInputComponent } from '../../components/top-menu/country-search-input/country-search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [],
+  imports: [CountrySearchInputComponent, CountryListComponent],
   templateUrl: './by-region-page.component.html',
 })
-export default class ByRegionPageComponent {}
+export default class ByRegionPageComponent {
+  onSearch(value: string) {
+    console.log(value);
+  }
+}
