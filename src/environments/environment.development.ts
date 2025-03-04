941be21b6408b03d635668a0c14b922b2c0642dd
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-04 14:28:19 +0100

    Agregar Google Maps

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
+  // API Keys
+  googleMapsApiKey: import.meta.env.NG_APP_GOOGLE_MAPS_API_KEY,
   // URLs
   restCountriesApiUrl: 'https://restcountries.com/v3.1',
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,46 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from '@environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+function loadGoogleMaps() {
+  const script = document.createElement('script');
+  script.innerHTML = `
+    (g => {
+      var h, a, k, p = "The Google Maps JavaScript API",
+          c = "google", l = "importLibrary", q = "__ib__",
+          m = document, b = window;
+      b = b[c] || (b[c] = {});
+      var d = b.maps || (b.maps = {}), r = new Set,
+          e = new URLSearchParams,
+          u = () => h || (h = new Promise(async (f, n) => {
+            await (a = m.createElement("script"));
+            e.set("libraries", [...r] + "");
+            for (k in g)
+              e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+            e.set("callback", c + ".maps." + q);
+            a.src = \`https://maps.\${c}apis.com/maps/api/js?\` + e;
+            d[q] = f;
+            a.onerror = () => h = n(Error(p + " could not load."));
+            a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+            m.head.append(a);
+          }));
+      d[l] ? console.warn(p + " only loads once. Ignoring:", g) :
+        d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n));
+    })({
+      v: "weekly",
+      key: "${environment.googleMapsApiKey}"
+    });
+  `;
+  document.head.appendChild(script);
+  console.log(
+    'Google maps loaded successfully with key',
+    environment.googleMapsApiKey
+  );
+}
+
+loadGoogleMaps();
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
