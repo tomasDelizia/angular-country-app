14690d54206596cf32b55e3fa6f631b27d8cf664
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-04 11:04:17 +0100

    Buscar países usando RxResource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -3,7 +3,8 @@ import { CountrySearchInputComponent } from '../../components/country-search-inp
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
-import { firstValueFrom } from 'rxjs';
+import { of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -14,17 +15,27 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
   query = signal<string>('');
 
-  // Utilizando recursos de Angular
-  countryResource = resource<Country[], any>({
-    // Se aconseja que los atributos de request sean señales para que el loader se vuelva a ejecutar si cambian
+  // Utilizando recursos de Rx
+  countryResource = rxResource<Country[], any>({
     request: () => ({ query: this.query() }),
-    loader: async ({ request }) => {
-      if (!request.query) return [];
-      return await firstValueFrom(
-        this.countryService.searchByCapital(request.query)
-      );
+    loader: ({ request }) => {
+      // Devolvemos un observable con un arreglo vacío
+      if (!request.query) return of([]);
+      return this.countryService.searchByCapital(request.query);
     },
   });
+
+  // Utilizando recursos de Angular
+  // countryResource = resource<Country[], any>({
+  //   // Se aconseja que los atributos de request sean señales para que el loader se vuelva a ejecutar si cambian
+  //   request: () => ({ query: this.query() }),
+  //   loader: async ({ request }) => {
+  //     if (!request.query) return [];
+  //     return await firstValueFrom(
+  //       this.countryService.searchByCapital(request.query)
+  //     );
+  //   },
+  // });
 
   // isLoading = signal(false);
   // hasError = signal<string | null>(null);
--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -3,7 +3,8 @@ import { CountrySearchInputComponent } from '../../components/top-menu/country-s
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { Country } from '../../interfaces/country.interface';
 import { CountryService } from '../../services/country.service';
-import { firstValueFrom } from 'rxjs';
+import { of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-by-country-page',
@@ -14,15 +15,21 @@ export default class ByCountryPageComponent {
   countryService = inject(CountryService);
   query = signal<string>('');
 
-  // Utilizando recursos de Angular
-  countryResource = resource<Country[], any>({
-    // Se aconseja que los atributos de request sean señales para que el loader se vuelva a ejecutar si cambian
+  countryResource = rxResource<Country[], any>({
     request: () => ({ query: this.query() }),
-    loader: async ({ request }) => {
-      if (!request.query) return [];
-      return await firstValueFrom(
-        this.countryService.searchByCountry(request.query)
-      );
+    loader: ({ request }) => {
+      if (!request.query) return of([]);
+      return this.countryService.searchByCountry(request.query);
     },
   });
+
+  // countryResource = resource<Country[], any>({
+  //   request: () => ({ query: this.query() }),
+  //   loader: async ({ request }) => {
+  //     if (!request.query) return [];
+  //     return await firstValueFrom(
+  //       this.countryService.searchByCountry(request.query)
+  //     );
+  //   },
+  // });
 }
