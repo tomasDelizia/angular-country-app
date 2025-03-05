e98c47baf4249d4d4d2e617ef4a075be214273f8
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-05 09:05:20 +0100

    Agregar debounce timer al buscador de países

--- src/app/country/components/country-search-input/country-search-input.component.ts
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,6 +8,18 @@ import { Component, input, output } from '@angular/core';
 export class CountrySearchInputComponent {
   placeholder = input<string>('Buscar');
   query = output<string>();
+  // Usamos una señal para almacenar el valor más actualizado del input
+  inputValue = signal<string>('');
+  debounceTime = input<number>(300);
+
+  // onCleanup es un callback que se ejecuta cuando se destruye el componente o cuando se redispara el efecto
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+    const timeout = setTimeout(() => {
+      this.query.emit(value);
+    }, this.debounceTime());
+    onCleanup(() => clearTimeout(timeout));
+  });
 
   onSearch(query: string) {
     if (query == '') return;
